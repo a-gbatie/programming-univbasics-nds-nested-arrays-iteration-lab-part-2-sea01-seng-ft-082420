5366181efd294bf446dbf7ea30f32b40f980cd55
Ange Batie <ange.batie@gmail.com>
2020-08-15 00:35:29 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,6 +5,15 @@ def find_min_in_nested_arrays(src)
     element_index = 0 
     inner_resluts = []
     lowest_value = 100
-  
+    while element_index < src[row_index].length do
+      if src[row_index][element_index] < lowest_value
+        lowest_value = src[row_index][element_index]
+      end
+      element_index += 1 
+    end
+    outer_results << lowest_value
+    row_index += 1 
+  end
+  outer_results
 
 end
